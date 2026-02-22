0ef73d8673e63b372db51cea96beaa8811caf53e
Ramesh <rameshpongiannan@gmail.com>
2026-02-22 20:39:27 +0800

    Created new Microservice using spring boot and spring cloud to deployment uisng docker and K8s

--- transaction-service/src/main/java/com/example/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/example/transactionservice/TransactionServiceApplication.java
@@ -5,10 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @SpringBootApplication
-@EnableFeignClients
 public class TransactionServiceApplication {
 
     private static final Logger log = LoggerFactory.getLogger(TransactionServiceApplication.class);
